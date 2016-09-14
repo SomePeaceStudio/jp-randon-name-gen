c7d30da1a0a3916d1cf60ecb243ce61669c78f5c
SomePeaceStudio <innuss22@gmail.com>
2016-09-14 14:04:34 +0300

    added random japanese hiragana generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,18 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: require('./app.component.html')
 })
-export class AppComponent { }
+export class AppComponent {
+  symbols = ['a','e','i','o','u','ka','ke','ki','ko','ku','sa','se','shi','so','su','ta','te','chi','to','tsu','na','ne','ni','no','nu'];
+  size = 5;
+  word = "";
+  generateWord(){
+    this.word = "";
+    let temp = ""
+    for (let i = 0; i < this.size; i++) {
+      // console.log(this.word);
+      temp += this.symbols[Math.floor((Math.random()*this.symbols.length))];
+    }
+    this.word = temp;
+    console.log(this.word);
+  }
+}
