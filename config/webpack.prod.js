72878c1f1d9d5735d6eb8b2ad6483a52505d0e52
SomePeaceStudio <innuss22@gmail.com>
2017-07-19 18:31:56 +0300

    Update webpack.prod.js
    Fixed link

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/manatelpa/', 
+    publicPath: '/jp-randon-name-gen/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
